d1879c1a1620e529ad2723c138fbce0c7a42e310
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-04-06 10:54:27 -0700

    removed macros

--- threadedSummation.c
+++ threadedSummation.c
@@ -5,14 +5,6 @@
 #include <time.h>
 #include <ctype.h>
 
-#define foreach(item, array) \
-    for(int keep = 1, \
-            count = 0,\
-            size = sizeof (array) / sizeof *(array); \
-        keep && count != size; \
-        keep = !keep, count++) \
-      for(item = (array) + count; keep; keep = !keep)
-
 long long int sum = 0;
 pthread_mutex_t lock;
 
